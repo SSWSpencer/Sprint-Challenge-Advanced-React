2ef5f0aaaacc6a64fde008028f934003c1688f7e
Steven Spencer <steve35spencer@gmail.com>
2020-04-10 13:36:17 -0400

    mvp

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render, fireEvent, wait } from "@testing-library/react";
 import App from './App';
+import PlayerGrid from "./components/PlayerGrid"
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+test('dark mode disabled by default', ()=> {
+  const results = render(<App />);
+  console.log(results.debug());
+})
+
+test('dark mode enable button works', () => {
+  const {getByText} = render(<App />);
+
+  const darkButton = getByText(/Toggle Dark Mode/i);
+  fireEvent.click(darkButton);
+})
+
+test('dark mode enabled after click', ()=> {
+  const results = render(<App />);
+  console.log(results.debug());
+})
+
+test('dark mode disable button works', () => {
+  const {getByText} = render(<App />);
+
+  const darkButton = getByText(/Toggle Dark Mode/i);
+  fireEvent.click(darkButton);
+})
---
+++ client/src/components/DarkModeButton.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react'
+import useDarkMode from "../hooks/useDarkMode";
+
+const DarkModeButton = () => {
+    const[darkMode, setDarkMode] = useDarkMode(false);
+    const toggleMode = e => {
+        e.preventDefault();
+        setDarkMode(!darkMode);
+      };
+      return(
+          <div>
+              <button onClick={toggleMode}>Toggle Dark Mode</button>
+          </div>
+      )
+}
+
+export default DarkModeButton;
